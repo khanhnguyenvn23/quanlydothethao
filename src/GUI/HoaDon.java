91e405b80fcaeac49335e91dbfad7b9b4a203928
Nguyen Cong Lam <nguyenconglam2k5@gmail.com>
2025-10-29 16:16:35 +0700

    test

---
+++ src/GUI/HoaDon.java
@@ -0,0 +1,15 @@
+package GUI;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.net.URL;
+
+public class HoaDon extends JPanel{
+    public HoaDon(){
+        setLayout(new BorderLayout());
+
+        setVisible(true);
+    }
+
+}
